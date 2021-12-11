c6dfc3f9b0d28f219a34f2e66b3d4a6956a842dd
pkarabiberis <61578516+pkarabiberis@users.noreply.github.com>
2021-12-11 15:29:53 +0200

    add labels & disable button if date inputs are bad

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -20,32 +20,54 @@ export const DatePicker: React.FC<DatePickerProps> = ({
   };
 
   return (
-    <div className="input-container">
-      <input
-        type="date"
-        name="from"
-        onChange={(e) =>
-          setStartDate(
-            new Date((e.target as HTMLInputElement).value).getTime() / 1000 //to unix
-          )
-        }
-      />
-      <input
-        type="date"
-        name="to"
-        max={formatMaxDate()}
-        onChange={(e) =>
-          setEndDate(
-            new Date((e.target as HTMLInputElement).value).getTime() / 1000 //to unix
-          )
-        }
-      />
+    <>
+      <div className="input-container">
+        <div>
+          <label
+            className={
+              startDate && endDate && startDate > endDate
+                ? 'label__from'
+                : undefined
+            }
+            htmlFor="from"
+          >
+            From
+          </label>
+          <input
+            type="date"
+            name="from"
+            id="from"
+            max={formatMaxDate()}
+            onChange={(e) =>
+              setStartDate(
+                new Date((e.target as HTMLInputElement).value).getTime() / 1000 //to unix
+              )
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="to">To</label>
+          <input
+            type="date"
+            name="to"
+            id="to"
+            max={formatMaxDate()}
+            onChange={(e) =>
+              setEndDate(
+                new Date((e.target as HTMLInputElement).value).getTime() / 1000 //to unix
+              )
+            }
+          />
+        </div>
+      </div>
+
       <button
         className={loading ? 'button--loading' : ''}
         onClick={handleSubmit}
+        disabled={!startDate || !endDate || startDate > endDate}
       >
         <span className="button__text">Find!</span>
       </button>
-    </div>
+    </>
   );
 };
